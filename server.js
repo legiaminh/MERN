53e54f4b659a89925e7f036712689431a17d36fe
Minh Le <legiaminh@gmail.com>
2019-10-08 16:14:36 -0700

    SecondCommit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
 
+connectDB();
 app.get('/', (req, res) => res.send('API is running'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
