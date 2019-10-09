1c9afaaba645f08605c2fa4067bfe958884b1a64
Minh Le <legiaminh@gmail.com>
2019-10-09 16:45:34 -0700

    Update More

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get('/', (req,res) => res.send('User route'));
+router.post('/', (req,res) => {
+    res.send('User route')
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const connectDB = require('./config/db');
 
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false}));
+
 app.get('/', (req, res) => res.send('API is running'));
 
 app.use('/api/users', require('./routes/api/users'));
