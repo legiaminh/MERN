f225306e843f62689e88c875ce60ada61eac389b
Minh Le <legiaminh@gmail.com>
2019-10-09 16:24:59 -0700

    UpdateModels

--- server.js
+++ server.js
@@ -4,5 +4,11 @@ const connectDB = require('./config/db');
 
 connectDB();
 app.get('/', (req, res) => res.send('API is running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
